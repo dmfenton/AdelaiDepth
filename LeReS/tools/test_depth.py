c4c2f0411b6bbd1a9fde73b567053dfa8c83b814
Daniel Fenton <dmfenton@gmail.com>
2021-07-03 13:18:47 -0400

    fix error when running in jupyter

--- LeReS/tools/test_depth.py
+++ LeReS/tools/test_depth.py
@@ -8,14 +8,18 @@ import argparse
 import numpy as np
 import torch
 
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Configs for LeReS')
-    parser.add_argument('--load_ckpt', default='./res50.pth', help='Checkpoint path to load')
-    parser.add_argument('--backbone', default='resnext101', help='Checkpoint path to load')
+    parser.add_argument('--load_ckpt', default='./res50.pth',
+                        help='Checkpoint path to load')
+    parser.add_argument('--backbone', default='resnext101',
+                        help='Checkpoint path to load')
 
     args = parser.parse_args()
     return args
+
 
 def scale_torch(img):
     """
@@ -28,7 +32,7 @@ def scale_torch(img):
         img = img[np.newaxis, :, :]
     if img.shape[2] == 3:
         transform = transforms.Compose([transforms.ToTensor(),
-		                                transforms.Normalize((0.485, 0.456, 0.406) , (0.229, 0.224, 0.225) )])
+                                        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
         img = transform(img)
     else:
         img = img.astype(np.float32)
@@ -51,17 +55,21 @@ if __name__ == '__main__':
     image_dir = os.path.dirname(os.path.dirname(__file__)) + '/test_images/'
     imgs_list = os.listdir(image_dir)
     imgs_list.sort()
-    imgs_path = [os.path.join(image_dir, i) for i in imgs_list if i != 'outputs']
+    imgs_path = [os.path.join(image_dir, i)
+                 for i in imgs_list if i != 'outputs']
     image_dir_out = image_dir + '/outputs'
     os.makedirs(image_dir_out, exist_ok=True)
 
     for i, v in enumerate(imgs_path):
+        if ".ipynb" in v:
+            continue
         print('processing (%04d)-th image... %s' % (i, v))
         rgb = cv2.imread(v)
         rgb_c = rgb[:, :, ::-1].copy()
         gt_depth = None
         A_resize = cv2.resize(rgb_c, (448, 448))
-        rgb_half = cv2.resize(rgb, (rgb.shape[1]//2, rgb.shape[0]//2), interpolation=cv2.INTER_LINEAR)
+        rgb_half = cv2.resize(
+            rgb, (rgb.shape[1]//2, rgb.shape[0]//2), interpolation=cv2.INTER_LINEAR)
 
         img_torch = scale_torch(A_resize)[None, :, :, :]
         pred_depth = depth_model.inference(img_torch).cpu().numpy().squeeze()
@@ -73,5 +81,7 @@ if __name__ == '__main__':
         img_name = v.split('/')[-1]
         cv2.imwrite(os.path.join(image_dir_out, img_name), rgb)
         # save depth
-        plt.imsave(os.path.join(image_dir_out, img_name[:-4]+'-depth.png'), pred_depth_ori, cmap='rainbow')
-        cv2.imwrite(os.path.join(image_dir_out, img_name[:-4]+'-depth_raw.png'), (pred_depth_ori/pred_depth_ori.max() * 60000).astype(np.uint16))
+        plt.imsave(os.path.join(image_dir_out,
+                                img_name[:-4]+'-depth.png'), pred_depth_ori, cmap='rainbow')
+        cv2.imwrite(os.path.join(image_dir_out, img_name[:-4]+'-depth_raw.png'),
+                    (pred_depth_ori/pred_depth_ori.max() * 60000).astype(np.uint16))
